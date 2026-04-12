7205309b53207a18ecb4aa9dddcf83c5cacff1bc
Van Trasmil <vtrasmil@gmail.com>
2026-04-12 20:55:07 +0800

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,5 @@
-const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
-const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
-const API_KEY = process.env.REACT_APP_API_KEY;;
 export const endpointPath = (country, category) =>
-  `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
+  `/api/news?country=${country}&category=${category}`;
+
 export const endpointSearch = (searchQuery) =>
-  `${API_SEARCH_DOMAIN}${searchQuery}&lang=en&apikey=${API_KEY}`;
+  `/api/news?q=${encodeURIComponent(searchQuery)}`;
