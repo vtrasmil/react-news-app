ef20008b41b605ddbeb9e1641a354486558f80fe
Van Trasmil <vtrasmil@gmail.com>
2026-04-12 22:28:43 +0800

    Update Article.js

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import "./Article.css";
+import "./Articles.css";
 
 function Article() {
   const { state } = useLocation();
