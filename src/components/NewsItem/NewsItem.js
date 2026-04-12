73e0171e364994d04c408634df19ae24d062f41d
Van Trasmil <vtrasmil@gmail.com>
2026-04-13 00:27:25 +0800

    Update NewsItem.js

--- src/components/NewsItem/NewsItem.js
+++ src/components/NewsItem/NewsItem.js
@@ -1,53 +1,74 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import Details from "./Details/Details";
 import { ReactComponent as ArrowIcon } from "../Images/ArrowIcon.svg";
+import "./NewsItem.css";
 
-function NewsItem({
-  title,
-  description,
-  content,
-  imageUrl,
-  urlNews,
-  channel,
-  published,
-}) {
+function NewsItem(props) {
+  const {
+    imageUrl,
+    alt,
+    description,
+    title,
+    channel,
+    published,
+    urlNews,
+    content,
+  } = props;
+
   const navigate = useNavigate();
 
-  const handleClick = () => {
+  const handleReadMore = () => {
     navigate("/article", {
       state: {
         title,
         description,
         content,
         url: urlNews,
-        image: imageUrl,
+        imageUrl,
         channel,
         published,
       },
     });
   };
 
-  const preview =
-    description ||
-    content?.slice(0, 180) ||
-    "No preview available";
-
   return (
-    <Card>
-      <Card.Img src={imageUrl} />
+    <Card className="card">
+      <Card.Img
+        className="card-img"
+        variant="top"
+        src={imageUrl}
+        alt={alt}
+      />
 
       <Card.Body>
         <Card.Title>{title}</Card.Title>
 
-        <Card.Text>{preview}</Card.Text>
+        <Card.Text className="card-description">
+          {description}
+        </Card.Text>
 
-        <Button onClick={handleClick}>
-          Read more <ArrowIcon />
+        <Details channel={channel} published={published} />
+
+        <Button className="card-btn" onClick={handleReadMore}>
+          Read more <ArrowIcon className="arrow-icon" />
         </Button>
       </Card.Body>
     </Card>
   );
 }
+
+NewsItem.propTypes = {
+  imageUrl: PropTypes.string,
+  alt: PropTypes.string,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  channel: PropTypes.string,
+  published: PropTypes.string,
+  urlNews: PropTypes.string,
+  content: PropTypes.string,
+};
 
 export default NewsItem;
