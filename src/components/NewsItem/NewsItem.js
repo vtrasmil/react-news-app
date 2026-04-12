21097d1bfe0615c572b14a34dd42dd938944ba18
Van Trasmil <vtrasmil@gmail.com>
2026-04-12 21:59:41 +0800

    Update NewsItem.js

--- src/components/NewsItem/NewsItem.js
+++ src/components/NewsItem/NewsItem.js
@@ -1,26 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import Details from "./Details/Details";
-import { ReactComponent as ArrowIcon } from '../Images/ArrowIcon.svg'; 
-import "./NewsItem.css"; 
+import { ReactComponent as ArrowIcon } from "../Images/ArrowIcon.svg";
+import "./NewsItem.css";
 
 function NewsItem(props) {
-  const { imageUrl, alt, description, title, channel, published, urlNews } =
-    props;
+  const {
+    imageUrl,
+    alt,
+    description,
+    title,
+    channel,
+    published,
+    urlNews,
+  } = props;
+
+  const navigate = useNavigate();
+
+  const handleReadMore = () => {
+    navigate("/article", {
+      state: { url: urlNews, title },
+    });
+  };
 
   return (
     <Card className="card">
       <Card.Img className="card-img" variant="top" src={imageUrl} alt={alt} />
+
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        <Card.Text className="card-description">{description}</Card.Text>
+
+        <Card.Text className="card-description">
+          {description}
+        </Card.Text>
+
         <Details channel={channel} published={published} />
-        <Button
-          className="card-btn"
-          href={urlNews}
-          target="_blank"
-        >
+
+        <Button className="card-btn" onClick={handleReadMore}>
           Read more <ArrowIcon className="arrow-icon" />
         </Button>
       </Card.Body>
