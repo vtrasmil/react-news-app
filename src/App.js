0b949487b62fcf928a9e4588b51ad8e0ec5730b3
Van Trasmil <vtrasmil@gmail.com>
2026-04-12 22:01:42 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,34 +2,38 @@ import React from "react";
 import { v4 as uuidv4 } from "uuid";
 import NavBar from "./components/NavBar/NavBar";
 import News from "./components/News/News";
+import Search from "./components/Search/Search";
+import Article from "./pages/Article"; // ✅ NEW IMPORT
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { router } from "./config/config";
-import Search from "./components/Search/Search";
 
 function App() {
   return (
-    <>
-      <Router>
-        <NavBar />
-        <Routes>
-          {router.map((path) => (
-            <Route
-              exact
-              key={uuidv4()}
-              path={path.path}
-              element={
-                <News
-                  key={path.key}
-                  newscategory={path.category}
-                  country={path.country}
-                />
-              }
-            />
-          ))}
-          <Route path="/search/:query" element={<Search />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <NavBar />
+
+      <Routes>
+        {router.map((path) => (
+          <Route
+            key={uuidv4()}
+            path={path.path}
+            element={
+              <News
+                newscategory={path.category}
+                country={path.country}
+              />
+            }
+          />
+        ))}
+
+        {/* Search route */}
+        <Route path="/search/:query" element={<Search />} />
+
+        {/* Article route (NEW) */}
+        <Route path="/article" element={<Article />} />
+      </Routes>
+    </Router>
   );
 }
 
